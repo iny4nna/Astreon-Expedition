7b2c690066c12796feabccbe8cc98cd4fb71b6c5
iny4nna <anna.naphachot@gmail.com>
2025-12-15 21:59:56 +0700

    Fix Python file extensions

---
+++ main.py
@@ -0,0 +1,6 @@
+from questions import questions
+
+print("Astreon Expedition")
+
+for q in questions:
+    print(q["question"])
---
+++ questions.py
@@ -0,0 +1,8 @@
+questions = [
+    {
+        "question": "Which planet is closest to the Sun?",
+        "choices": ["Earth", "Mars", "Mercury", "Venus"],
+        "answer": 2
+    }
+]
+
